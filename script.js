0e69bba8e842d0bf0b8872f3a64de6d6fe7ae943
AlecJoe <alecjoe1312@gmail.com>
2023-10-25 01:08:28 -0400

    7
    어려워 기호야..

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-function greet(name) {
-  console.log("Hello" + name);
-}
-greet("Kiho");
+let searchBar = document.getElementById("Search");
+let Container = document.getElementById("Container");
+let inputfield = document.getElementById("inputField");
 
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
-circle.draw();
-
-const circle1 = new circle(10);
-for (let key in cirle) {
-  console.log(key, circle[key]);
-}
-const keys = object.keys(circle);
-console.log(keys);
-if ("radius" in circle) console.log("circle has a radius.");
+searchBar.addEventListene("click", function () {
+  var paragraph = document.createElement("p");
+  paragraph.innertext = inputfield.value;
+  window.open("http://google.com/" + inputfield.value);
+});
