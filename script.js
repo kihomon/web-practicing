407fcbccf76950f32b2212851235667252088b1f
AlecJoe <alecjoe1312@gmail.com>
2023-10-25 01:22:29 -0400

    8

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let searchBar = document.getElementById("Search");
 let Container = document.getElementById("Container");
 let inputfield = document.getElementById("inputField");
 
-searchBar.addEventListene("click", function () {
+searchBar.addEventListener("click", function () {
   var paragraph = document.createElement("p");
   paragraph.innertext = inputfield.value;
-  window.open("http://google.com/" + inputfield.value);
+  window.open("http://google.com/search?q=" + inputfield.value);
 });
